33cb243a079128e343b0c0d7e4a6d10e8a86a14a
Jey Puget Gil <44286896+JPugetGil@users.noreply.github.com>
2024-12-20 22:43:55 +0000

    Fix: Markers are now included inside the bundle

---
+++ src/leaflet-marker-config.ts
@@ -0,0 +1,11 @@
+// https://cescobaz.com/2023/06/14/setup-leaflet-with-svelte-and-vite/
+import 'leaflet/dist/leaflet.css';
+import * as L from 'leaflet'
+import markerIconUrl from 'leaflet/dist/images/marker-icon.png';
+import markerIconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+L.Icon.Default.prototype.options.iconUrl = markerIconUrl;
+L.Icon.Default.prototype.options.iconRetinaUrl = markerIconRetinaUrl;
+L.Icon.Default.prototype.options.shadowUrl = markerShadowUrl;
+L.Icon.Default.imagePath = "";
